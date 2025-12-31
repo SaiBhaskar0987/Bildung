d53b24016b181df81bb1c8cc68c352b3c5f6982f
sindhura231995-maker <sindhura231995@gmail.com>
2025-12-31 14:51:42 +0530

    added client,secret key

--- core/asgi.py
+++ core/asgi.py
@@ -33,3 +33,14 @@ application = ProtocolTypeRouter({
     ),
 })
 
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from chat.routing import websocket_urlpatterns
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(websocket_urlpatterns),
+})
